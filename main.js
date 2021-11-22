b2fa4b19c8394051669a9d5224f815ce6e484d94
Karishma <kdaya001@gmail.com>
2021-11-22 20:53:42 +1100

    Add game state storage in an Array and CSS animation

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const PLAYERS = {
     playerOne: {
-        name: "Player One",
+        name: 'Player One',
         token: 'X',
     },
     playerTwo: {
-        name: "Player Two",
+        name: 'Player Two',
         token: 'O',
     },
 };
@@ -20,31 +20,39 @@ const WINNING_CONDITIONS = [
     [2, 4, 6],
 ];
 
+const currentGameState = [null, null, null, null, null, null, null, null, null];
+
 const bottomBanner = document.getElementById('bottom-banner');
 
 let winningState = false;
 
-let activePlayer = PLAYERS.playerOne.token;
+let activePlayerToken = PLAYERS.playerOne.token;
 
+//Event listener for if the Reset Board button is clicked
 const resetBoardButton = document.getElementById('reset-board');
 resetBoardButton.addEventListener('click', function () {
     resetBoard();
 });
 
+//event listener for if Update Player names is selected
 const changeName = document.getElementById('submit-name');
 changeName.addEventListener('click', function (event) {
-    const playerOneNameInput = document.getElementById('player-one-chosen-name');
-    const playerTwoNameInput = document.getElementById('player-two-chosen-name');
+    const playerOneNameInput = document.getElementById(
+        'player-one-chosen-name'
+    );
+    const playerTwoNameInput = document.getElementById(
+        'player-two-chosen-name'
+    );
 
-    if(event.target.textContent === "Update Player Names") {
+    if (event.target.textContent === 'Update Player Names') {
         playerOneNameInput.removeAttribute('type', 'hidden');
         playerTwoNameInput.removeAttribute('type', 'hidden');
 
-        event.target.textContent = "Update";
+        event.target.textContent = 'Update';
     } else {
         if (playerOneNameInput.value !== '' && playerTwoNameInput !== '') {
             let currentPlayer = document.getElementById('active-player');
-            if(currentPlayer.textContent === PLAYERS.playerOne.name) {
+            if (currentPlayer.textContent === PLAYERS.playerOne.name) {
                 currentPlayer.textContent = playerOneNameInput.value;
             } else if (currentPlayer.textContent === PLAYERS.playerTwo.name) {
                 currentPlayer.textContent = playerTwoNameInput.value;
@@ -52,22 +60,26 @@ changeName.addEventListener('click', function (event) {
             PLAYERS.playerOne.name = playerOneNameInput.value;
             PLAYERS.playerTwo.name = playerTwoNameInput.value;
 
-            document.getElementById('player-one-name').textContent = PLAYERS.playerOne.name;
-            document.getElementById('player-two-name').textContent = PLAYERS.playerTwo.name;
+            document.getElementById('player-one-name').textContent =
+                PLAYERS.playerOne.name;
+            document.getElementById('player-two-name').textContent =
+                PLAYERS.playerTwo.name;
 
             playerOneNameInput.setAttribute('type', 'hidden');
             playerTwoNameInput.setAttribute('type', 'hidden');
             playerOneNameInput.value = '';
             playerTwoNameInput.value = '';
-            event.target.textContent = "Update Player Names";
+            event.target.textContent = 'Update Player Names';
         } else {
-            alert('Enter a valid entry');
+            alert('Enter a valid entry for both players');
         }
     }
 });
 
+
+//Event listener for if reset score is selected
 const resetScore = document.getElementById('reset-score');
-resetScore.addEventListener('click', function() {
+resetScore.addEventListener('click', function () {
     const playerOneScore = document.getElementById('player-one-points');
     const playerTwoScore = document.getElementById('player-two-points');
     playerOneScore.textContent = 0;
@@ -75,6 +87,7 @@ resetScore.addEventListener('click', function() {
     resetBoard();
 });
 
+//event listener to add click function for the game board
 const boxWrapper = document.querySelector('#wrapper');
 boxWrapper.addEventListener('click', function (event) {
     if (event.target.classList.contains('box')) {
@@ -84,24 +97,32 @@ boxWrapper.addEventListener('click', function (event) {
     }
 });
 
+//function to assign value to the board AND game state
 function assignChosenValue(event) {
     if (event.target.textContent === '') {
         const para = document.createElement('p');
-        const node = document.createTextNode(activePlayer);
-        para.classList.add('displayValue');
+        const node = document.createTextNode(activePlayerToken);
+
+        let num = event.target.id.slice(-2);
+        num = Number(num);
+        currentGameState[num - 1] = activePlayerToken;
+
         para.appendChild(node);
+        para.classList.add('displayValue');
+        para.classList.toggle('box-animation');
         event.target.appendChild(para);
-        checkWinCondition();
+        checkGameEndConditions();
     }
 }
 
-function checkWinCondition() {
+
+function checkGameEndConditions() {
     const boxes = document.querySelectorAll('.box');
-    if (checkEndWinningState(boxes)) {
+    if (checkEndWinningState(boxes, currentGameState)) {
         setTimeout(function () {
-            endGame(activePlayer);
+            endGame(activePlayerToken);
         }, 1);
-    } else if (checkDrawState(boxes)) {
+    } else if (checkDrawState(currentGameState)) {
         setTimeout(function () {
             endGame();
         }, 1);
@@ -110,12 +131,12 @@ function checkWinCondition() {
     }
 }
 
-function checkEndWinningState(boxes) {
+function checkEndWinningState(boxes, currentGameState) {
     for (condition of WINNING_CONDITIONS) {
         if (
-            boxes[condition[0]].textContent === activePlayer &&
-            boxes[condition[1]].textContent === activePlayer &&
-            boxes[condition[2]].textContent === activePlayer
+            currentGameState[condition[0]] === activePlayerToken &&
+            currentGameState[condition[1]] === activePlayerToken &&
+            currentGameState[condition[2]] === activePlayerToken
         ) {
             changeColor(condition, boxes);
             return true;
@@ -132,18 +153,18 @@ function changeColor(condition, boxes) {
 
 function changeActivePlayer() {
     const activePlayerUI = document.querySelector('#active-player');
-    if (activePlayer === PLAYERS.playerOne.token) {
-        activePlayer = PLAYERS.playerTwo.token;
+    if (activePlayerToken === PLAYERS.playerOne.token) {
+        activePlayerToken = PLAYERS.playerTwo.token;
         activePlayerUI.textContent = 'Player Two';
     } else {
-        activePlayer = PLAYERS.playerOne.token;
+        activePlayerToken = PLAYERS.playerOne.token;
         activePlayerUI.textContent = 'Player One';
     }
 }
 
 function checkDrawState(boxes) {
     for (index in boxes) {
-        if (boxes[index].textContent === '') {
+        if (boxes[index] === null) {
             return false;
         }
     }
@@ -171,7 +192,7 @@ function updatePoints(winningPlayer) {
     const playerOnePoints = document.getElementById('player-one-points');
     const playerTwoPoints = document.getElementById('player-two-points');
     let points;
-    if(winningPlayer === 'playerOne') {
+    if (winningPlayer === 'playerOne') {
         points = Number(playerOnePoints.textContent) + 1;
         playerOnePoints.textContent = points;
     } else if (winningPlayer === 'playerTwo') {
@@ -180,15 +201,21 @@ function updatePoints(winningPlayer) {
     }
 }
 
+//function to reset the board
 function resetBoard() {
     const boxes = document.querySelectorAll('.box');
     for (box of boxes) {
         box.textContent = '';
         box.style.color = null;
     }
-    bottomBanner.innerHTML = 'Current Player: <span id="active-player">PlayerOne</span>';
+    bottomBanner.innerHTML =
+        'Current Player: <span id="active-player">PlayerOne</span>';
     bottomBanner.style.color = null;
     bottomBanner.style.fontSize = null;
-    activePlayer = PLAYERS.playerOne.token;
+    activePlayerToken = PLAYERS.playerOne.token;
     winningState = false;
+
+    for (let index in currentGameState) {
+        currentGameState[index] = null;
+    }
 }
